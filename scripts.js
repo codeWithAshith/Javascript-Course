bca132aa023901e586f865f374163b26ea4afa06
ashithvl <ashithvl@gmail.com>
2022-08-30 10:58:53 +0530

    1. Variables

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+// default value is undefined
+// let name;
+// console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain space or hypen(-)
+// Are case sensitive
+// 
+
+let name = 'CodeWithAshith'
+console.log(name);
+
