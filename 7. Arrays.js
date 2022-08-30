2f4fbf267175dd4119c7249f9be4d47bfc787f42
ashithvl <ashithvl@gmail.com>
2022-08-30 11:53:17 +0530

    7._Arrays

---
+++ 7. Arrays.js
@@ -0,0 +1,21 @@
+let selectedColor = ["red", "blue"];
+
+// console.log(selectedColor);
+// console.log(selectedColor[0]);
+
+// // js is an dynamic language
+// // the length of an array can be increased
+
+// selectedColor[2] = "green";
+// console.log(selectedColor);
+
+// js arrays can have different type of data
+// selectedColor[3] = 3;
+// console.log(selectedColor);
+
+// in js array is of type object
+// console.log(typeof(selectedColor));
+
+console.log(selectedColor.length);
+
+// array is an data structure that is used to represent a list of numbers
