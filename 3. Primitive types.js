15e27a76366ea57f7e21e3f8b24f9eaa24c81b2d
ashithvl <ashithvl@gmail.com>
2022-08-30 11:26:51 +0530

    3. Primitive types

---
+++ 3. Primitive types.js
@@ -0,0 +1,4 @@
+let name = 'codewithashith'; // string literal
+let age = 40; // number literal
+let isApproved = false // boolean literal
+let firstName = null; // used in situation where we wanna clear the value of a variable
