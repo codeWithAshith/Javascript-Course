4d282e569a17e20a84c9612482b4ab791ceca88d
ashithvl <ashithvl@gmail.com>
2022-08-30 11:42:34 +0530

    4. Dynamic language

---
+++ 4. Dynamic language.js
@@ -0,0 +1,14 @@
+// the type of a variable will be defined in run time
+// let name = "code with Ashith";
+// console.log(typeof name);
+
+// name = 10;
+// console.log(name);
+
+// no floating point number
+// undefined is a type
+// null is an object
+
+let color = null;
+console.log(color);
+ 
