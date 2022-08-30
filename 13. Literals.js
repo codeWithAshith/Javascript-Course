a2cae122030f80271d6d1e6b2098fb3037a1df9b
ashithvl <ashithvl@gmail.com>
2022-08-30 14:59:33 +0530

    13._Literals

---
+++ 13. Literals.js
@@ -0,0 +1,13 @@
+let firstName = "code";
+let middleName = "with";
+let lastName = "Ashith";
+
+const fullName = firstName + " " + middleName + " " + lastName;
+
+console.log(fullName);
+
+// using string literal
+const fullNameUsingLiteral = `${firstName} ${middleName} ${lastName}`;
+console.log(fullNameUsingLiteral);
+
+
