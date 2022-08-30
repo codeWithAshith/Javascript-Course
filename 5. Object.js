d99c5efb46c911cf40a7f5a02ab53ef0bdd266e8
ashithvl <ashithvl@gmail.com>
2022-08-30 11:47:55 +0530

    5. Objects

---
+++ 5. Object.js
@@ -0,0 +1,22 @@
+let name = "code with Ashith";
+let age = 40;
+
+let person = {
+  name: "Mosh",
+  age: "40",
+};
+
+console.log(person);
+
+//dot notation
+person.name = "Ashith";
+console.log(person.name);
+
+//bracket notation
+person["name"] = "Ashi";
+
+//dynamic selection
+let selection = "name";
+person[selection] = 'Ashi'
+
+console.log(person.name);
