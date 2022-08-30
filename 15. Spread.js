c3c1a71c742fd3318559441e43c7981d1ac90fba
ashithvl <ashithvl@gmail.com>
2022-08-30 15:18:13 +0530

    15._Spread

---
+++ 15. Spread.js
@@ -0,0 +1,11 @@
+let exampleOne = [1, 2, 3, 4, 5];
+
+let exampleTwo = [...exampleOne];
+
+console.log(exampleTwo);
+
+const numbersOne = [1, 2, 3];
+const numbersTwo = [4, 5, 6];
+const numbersCombined = [...numbersOne, ...numbersTwo];
+
+console.log(numbersCombined);
