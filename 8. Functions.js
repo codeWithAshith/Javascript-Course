26ce778bd99a92e86c2f1c28adfb5ab672b5f81b
ashithvl <ashithvl@gmail.com>
2022-08-30 12:00:12 +0530

    8. Functions

---
+++ 8. Functions.js
@@ -0,0 +1,29 @@
+// fundamental building blocks in js
+// a set of statement that performs a task
+
+function greet() {
+  console.log("Hello World");
+}
+
+greet();
+
+//arrow function
+const greetArrowFunction = () => {
+  console.log("Hello World");
+};
+
+greetArrowFunction();
+
+function greetWithName(name) {
+  console.log("Hello " + name);
+}
+
+greetWithName("John");
+
+//arrow function
+const greetWithNameArrowFunction = (name) => {
+  console.log("Hello " + name);
+};
+
+greetWithNameArrowFunction();
+
