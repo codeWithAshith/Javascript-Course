ab03a761a96ee4358d8f7ff48aa35ae0c543b964
codewithashith <codewithashith@gmail.com>
2022-10-27 13:21:18 +0530

    25. FindIndex

---
+++ 25. FindIndex.js
@@ -0,0 +1,17 @@
+// The findIndex() method returns the index of the first array element
+// that satisfies the provided test function or else returns -1.
+
+// function that returns odd number
+function isOdd(element) {
+  return element % 2 !== 0;
+}
+
+// defining an array of integers
+let numbers = [2, 8, 1, 3, 4];
+
+// returns the index of the first odd number in the array
+let firstOdd = numbers.findIndex(isOdd);
+
+console.log(firstOdd);
+
+// Output: 2
